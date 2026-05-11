71e5343f0a6d27b66f8d48b1e1b7140b82031ea8
mohadaahir <mohadaahir2023@gmail.com>
2026-05-11 21:39:22 +0300

    exercise 5

---
+++ exercise 5/arrow.js
@@ -0,0 +1,4 @@
+const add=(a,b)=>{
+    return a+b
+}
+console.log(add(55,66))
---
+++ practice.code/practice.js
@@ -0,0 +1,17 @@
+function great(){
+    console.log("hello everyone")
+}
+great()
+
+function salaan(name){
+    console.log("hello "+name)
+}
+salaan("cali")
+salaan("farax")
+
+function add(a,b){
+    return a+b
+}
+console.log(add(40,2))
+let result=add(30,10)
+console.log(result/2)
