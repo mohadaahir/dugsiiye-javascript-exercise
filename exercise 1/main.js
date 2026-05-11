138708a5afdbb8e16aececf2a508af935f60a91b
mohadaahir <mohadaahir2023@gmail.com>
2026-05-11 06:52:13 +0300

    exercise1

---
+++ exercise 1/main.js
@@ -0,0 +1,14 @@
+let x =7;
+let y=3
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+//pre-increment
+console.log("pre-incremt")
+console.log(++y)
+//post
+console.log("post-incrment")
+console.log(x++)
+console.log(x)
