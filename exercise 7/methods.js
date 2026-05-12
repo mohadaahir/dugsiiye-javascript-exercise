91bc6eb661b7842add10175dae506cfa7e79047f
mohadaahir <mohadaahir2023@gmail.com>
2026-05-12 21:14:25 +0300

    done

---
+++ exercise 7/methods.js
@@ -0,0 +1,11 @@
+let colers=["red","blue","yellow","orange"]
+console.log(colers)
+colers.push("green")
+ console.log(colers)
+colers.pop()
+console.log(colers)
+
+colers.unshift("pink")
+ console.log(colers)
+  colers.shift()
+ console.log(colers)
