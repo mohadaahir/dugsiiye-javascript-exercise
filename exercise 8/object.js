698a44943adcf6bd07f7cd63cc38e90d18c95743
mohadaahir <mohadaahir2023@gmail.com>
2026-05-16 20:33:32 +0300

    exercise 9

--- exercise 8/object.js
+++ exercise 8/object.js
@@ -1,8 +1,24 @@
 let car={
     model:"herri",
     year:1990,
-    make:"toyoto"
+    make:"toyoto",
+    start:function(){
+        console.log("the car has started")
+    }
 }
+car.start(),
 console.log(car.make)
 console.log(car.year)
 console.log(car.model)
+// let university={
+//     name:"siu",
+//     location:"baar ubax",
+//     date:1990,
+
+// }
+// console.log(university.name);
+
+// university.class=34
+
+// // university.name=34,
+// console.log(university);
---
+++ exercise 9/object.js
@@ -0,0 +1,12 @@
+let car={
+    model:"herri",
+    year:1990,
+    make:"toyoto",
+    start:function(){
+        console.log("the car has started")
+    }
+}
+car.start(),
+console.log(car.make)
+console.log(car.year)
+console.log(car.model)
