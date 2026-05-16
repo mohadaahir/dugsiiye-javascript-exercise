afa48d2d97e0c47c02d7a59176da9ee243010de7
mohadaahir <mohadaahir2023@gmail.com>
2026-05-16 21:16:56 +0300

    exercise 10

---
+++ exercise 10/arrowObject.js
@@ -0,0 +1,22 @@
+let libariry=[
+    {
+        name:"waan ku salem sxp",
+        author:"adan sahanka",
+        title:"naftaa bar"
+    },
+    {
+        name:"naftaa gacalo",
+        author:"mohamed",
+        title:"naftaa bar"
+    },
+    {
+        name:"waan ku salem sxp",
+        author:"sahanka",
+        title:" bar"
+    }
+]
+console.log(libariry);
+console.log(libariry[1]);
+console.log(libariry[2]);
+
+
