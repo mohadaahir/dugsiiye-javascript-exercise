ee3d0237b602966e46efe6a32ba380f4e008b393
mohadaahir <mohadaahir2023@gmail.com>
2026-05-23 21:37:17 +0300

    exercise 15

---
+++ exercise 15/for-in-of.js
@@ -0,0 +1,13 @@
+
+let peaple=[
+    {name:"mohamed",age:23,city:"muqdisho"},
+    {name:"cali",age:44,city:"bososo"},
+    {name:"farax",age:23,city:"garoowe"},
+];
+for(const person of peaple){
+for(key in person){
+    console.log(key +"  "+person[key]);
+    
+}
+console.log("...........")
+}
