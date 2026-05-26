d748e7f1a98e6aeec45b47a1efbff3a7fcb9fd76
mohadaahir <mohadaahir2023@gmail.com>
2026-05-26 07:13:42 +0300

    exercise 21

--- exercise 20/map.js
+++ exercise 20/map.js
@@ -1,3 +1,8 @@
 let fruits=["apple","banana","cherry"]
 const newfruits=fruits.map(fruit=>fruit.length)
 console.log(" length" +newfruits);
+
+
+
+
+
---
+++ exercise 21/reduce.js
@@ -0,0 +1,11 @@
+let number=[1,2,3,4,5,6,7,8,9,10];
+const addnum=number.reduce((total,numb)=>total+numb,0)
+console.log(addnum);
+console.log("-----------------------------------------");
+
+ let score=[40,50,60,70,80,90]
+let passed=score.filter((result)=>result>=60);
+console.log(passed);
+let numbers=[1,2,3,4,5,6,7,8,9]
+let evennumber=numbers.filter((even)=>even%2===0)
+console.log(evennumber);
