03c85a80a75090599e350ed4c0edc162ea4ea0d4
mohadaahir <mohadaahir2023@gmail.com>
2026-06-18 05:33:54 +0300

    exercise 32

--- exericse 32/selector.js
+++ exericse 32/selector.js
@@ -1,11 +1,12 @@
-const head=document.querySelector('head')
+const head=document.querySelector('#head')
 console.log(head);
-const describtion=document.querySelectorAll('text')
+const describtion=document.querySelectorAll('.text')
 console.log(describtion);
-const name=document.querySelector("name")
-console.log('name');
-const button=document.querySelectorAll("btn")
-console.log("button");
-const paragarph=document.querySelector("paragarph")
-console.log("paragarph");
+const name=document.querySelector("#name")
+console.log(name);
+const button=document.querySelectorAll(".btn")
+console.log(button);
+const p=document.querySelector("#paragarph")
+console.log(p);
+
 
