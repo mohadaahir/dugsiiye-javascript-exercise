468e655e845962b8a66acdc16ae27f9f9ca2e57e
mohadaahir <mohadaahir2023@gmail.com>
2026-06-18 07:32:35 +0300

    exercise 33

---
+++ exercise 33/portfile.js
@@ -0,0 +1,11 @@
+function changebutton(){
+    const hero=document.querySelector('#project')
+    const main=document.querySelector('h3')
+
+    main.textContent=' I am full-stack development'
+}
+function contect(){
+    const text=document.querySelector('.web')
+console.log(text)
+    text.textContent='just click'
+}
--- exericse 32/selector.js
+++ exericse 32/selector.js
@@ -1,12 +1,17 @@
 const head=document.querySelector('#head')
 console.log(head);
-const describtion=document.querySelectorAll('.text')
+const describtion=document.querySelector('.text')
 console.log(describtion);
 const name=document.querySelector("#name")
 console.log(name);
-const button=document.querySelectorAll(".btn")
+const button=document.querySelector(".btn")
 console.log(button);
 const p=document.querySelector("#paragarph")
 console.log(p);
+function changeContent(){
+    head.textContent='firts dom i can make'
+}
 
-
+function changeCont(){
+            describtion.innerHTML = "This content has been <strong>updated</strong>.";
+}
