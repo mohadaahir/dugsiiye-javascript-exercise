ad2d37e3a57c3293aeb88fd90d8f861c702d3f5b
mohadaahir <mohadaahir2023@gmail.com>
2026-08-13 16:16:32 +0300

    exericse 13

--- exercise 13/while.js
+++ exercise 13/while.js
@@ -1,25 +1,24 @@
 let x=5;
 while(x>=1){
     console.log(x);
-    x--
-    
-}
-let counter = 10;
-while (counter > 0) {
-    console.log("counter"+counter);
-    counter--;
-}
+    x--}
+// }
+// let counter = 10;
+// while (counter > 0) {
+//     console.log("counter"+counter);
+//     counter--;
+// }
 
 
- let i =10;
- do{
-   console.log(i)
-    i--
-}while(i>0)
+//  let i =10;
+//  do{
+//    console.log(i)
+//     i--
+// }while(i>0)
 
-let password
-do{
-    password=prompt("enter password")
+// let password
+// do{
+//     password=prompt("enter password")
 
-}while(password !=="123")
-console.log("admin")
+// }while(password !=="123")
+// console.log("admin")
